a5d2996f332a9d04f856057d6f63e9c33fbae5fb
Ivangcode <ivangm180@gmail.com>
2023-04-19 13:25:18 -0600

    add update task view

--- tasks/views.py
+++ tasks/views.py
@@ -72,11 +72,35 @@ def tasks(request):
 
 
 def tasks_detail(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
-    print(task)
-    return render(
-        request, "task_detail.html", {"task": task}
-    )
+    if request.method == "GET":
+        task = get_object_or_404(
+            Task, pk=task_id, user=request.user
+        )
+        form = TaskForm(instance=task)
+        print(task)
+        return render(
+            request,
+            "task_detail.html",
+            {"task": task, "form": form},
+        )
+    else:
+        try:
+            task = get_object_or_404(
+                Task, pk=task_id, user=request.user
+            )
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect("tasks")
+        except ValueError:
+            return render(
+                request,
+                "task_detail.html",
+                {
+                    "task": task,
+                    "form": form,
+                    "error": "Error updating task",
+                },
+            )
 
 
 def logoutSession(request):
